f955965a98dd2afd875e551f356bc796b3339497
Madiha Khan <159880897+khan786madiha@users.noreply.github.com>
2024-03-06 09:13:55 -0800

    Q-36 Ts/Js & node.js of 45 assignments.
    Madiha Khan's NODEJS 45 Assignment Q-36

---
+++ Q36.js
@@ -0,0 +1,10 @@
+"use strict";
+// 36.	T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+Object.defineProperty(exports, "__esModule", { value: true });
+function make_shirt(size, message) {
+    console.log(`I have a ${size} size T-shirt, message on the T-shirt"${message}"`);
+}
+// Calling the function
+make_shirt("Small", "This T-shirt is too small for me!");
+make_shirt("Large", "This T-shirt is perfect for me!");
+make_shirt("Large", "This T-shirt is too large for me!");
